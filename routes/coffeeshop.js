2d9edc606dbc73646dd333cc0c759fc8590ddce7
tudatn <tudatn@sfu.ca>
2018-07-10 17:03:49 -0700

    reorganize project skeleton and add roomController

--- routes/coffeeshop.js
+++ routes/coffeeshop.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var spotify_controller = require('../controllers/spotifyController');
+var room_controller = require('../controllers/roomController');
 
 /* GET home page. */
-router.get('/', spotify_controller.search_get);
+// router.get('/', spotify_controller.search_get);
+router.get('/', room_controller.index);
 
-router.post('/', spotify_controller.search_post);
+// router.post('/', spotify_controller.search_post);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.redirect('/coffeeshop');
-=======
-  res.render('index', { title: 'Coffee Shop' });
->>>>>>> b78f10573f8f525c1705475332db4efbdf3cb8e2
 });
 
 module.exports = router;

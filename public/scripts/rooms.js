0827a1ec4e9640d6bdf2a671eecf1ec92bc7a357
Alexis Lizardo <alizardo@sfu.ca>
2018-07-19 12:27:23 -0700

    Modified socket io to have per room differentiation

--- public/scripts/rooms.js
+++ public/scripts/rooms.js
@@ -1,11 +1,19 @@
 $(function() {
   const socket = io('/rooms', {transports: ['websocket'], upgrade: false});
-  
+
+  socket.on('connect', () => {
+    console.log("connected");
+    socket.emit('join room', room_id);
+  })
+
   socket.on('user joined room', (data) => {
+      console.log("room joined");
       $('#num-users').text(data.roomUsers);
   })
 
   socket.on('user left room', (data) => {
+      console.log("left room");
       $('#num-users').text(data.roomUsers);
   })
+
 })

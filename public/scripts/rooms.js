a8fc3b6381e7c1c8743fdae8b462267663caf1ca
Adam Tuck <tuckaet@gmail.com>
2018-07-20 03:18:55 -0700

    realtime adding and deletion of dom elements per room

--- public/scripts/rooms.js
+++ public/scripts/rooms.js
@@ -6,22 +6,38 @@ $(function() {
     socket.emit('join room', room_id);
   });
 
+  // data is array of client IDs
   socket.on('user joined room', (data) => {
     console.log("client joined room...");
     $('#num-users').text(data.length);
     data.forEach((client) => {
-      console.log(`appending ${client}...`);
-      $('#connected-users').append( $(`<p#${client}>${client}</p>`))
+      client = client.replace('/rooms#', '');
+      if (!$(`#${client}`).length){
+        $('#connected-users').append( $(`<p class='socketid' id=${client}>${client}</p>`))
+      }
     })
   });
 
+  // data is array of client IDs
   socket.on('user left room', (data) => {
     console.log("client left room...");
     $('#num-users').text(data.length);
+    // data and DOM elements are not same order
     data.forEach((client) => {
-      console.log(`removing ${client}...`);
-      let el = $('#connected-users').find( $(`#${client}`))
-      if (!el) $(`#${client}`).remove();
+      client = client.replace('/rooms#', '');
+      $('.socketid').each( (i, obj) => {
+        if (client == $(obj).attr('id')){
+          $(obj).addClass('safe');
+        }
+      });
     })
+    // second loop over initial check to do deletion
+    $('.socketid').each( (i, obj) => {
+      if (!($(obj).hasClass('safe'))){
+        $(obj).remove();
+        return;
+      }
+      $(obj).removeClass('safe');
+    });
   });
 })

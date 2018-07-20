70232bb42e7d534db5861035a94aa14b6e5bd702
Adam Tuck <tuckaet@gmail.com>
2018-07-19 22:29:37 -0700

    per-room-sockets w/ IDs working

--- public/scripts/rooms.js
+++ public/scripts/rooms.js
@@ -2,18 +2,26 @@ $(function() {
   const socket = io('/rooms', {transports: ['websocket'], upgrade: false});
 
   socket.on('connect', () => {
-    console.log("connected");
+    console.log('client connected, joining room...');
     socket.emit('join room', room_id);
-  })
+  });
 
   socket.on('user joined room', (data) => {
-      console.log("room joined");
-      $('#num-users').text(data.roomUsers);
-  })
+    console.log("client joined room...");
+    $('#num-users').text(data.length);
+    data.forEach((client) => {
+      console.log(`appending ${client}...`);
+      $('#connected-users').append( $(`<p#${client}>${client}</p>`))
+    })
+  });
 
   socket.on('user left room', (data) => {
-      console.log("left room");
-      $('#num-users').text(data.roomUsers);
-  })
-
+    console.log("client left room...");
+    $('#num-users').text(data.length);
+    data.forEach((client) => {
+      console.log(`removing ${client}...`);
+      let el = $('#connected-users').find( $(`#${client}`))
+      if (!el) $(`#${client}`).remove();
+    })
+  });
 })

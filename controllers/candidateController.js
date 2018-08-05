ac81bdbbe1aa4e8c520dd507ae8faa8ee39dd8fe
tudatn <tudatn@sfu.ca>
2018-08-04 22:45:19 -0700

    modified candidate_create_post to call directly from server

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -129,37 +129,29 @@ exports.downvoteCandidate = [
 
 // Display Contact create form on POST.
 exports.candidate_create_post = function(candidate) {
-    // From Adam: Structure of candidate object
-    console.log('request '+JSON.stringify(candidate));
 
-    // From Adam: Don't need this anymore because we have the object
-    // var data = {
-    //     id: candidate.id,
-    //     uri: candidate.uri,
-    //     song: candidate.song,
-    //     artist: candidate.artist,
-    //     album_name: candidate.album_name,
-    //     album_image: candidate.album_image,
-    //     votes: candidate.votes,
-    //     room: candidate.room
-    // };
+    var data = {
+        uri: candidate.uri,
+        name: candidate.song,
+        artist: candidate.artist,
+        album_name: candidate.album_name,
+        album_image: candidate.album_image,
+        room: candidate.room
+    };
 
-    console.log(req.body.uri);
     // find or create a new song
-    // From Adam: Make sure this actually does what it's supposed to now that value names are different
     models.Song.findOrCreate({
-        where: { id: candidate.id },
-        defaults: candidate
+        where: { uri: candidate.uri },
+        defaults: data
     }).spread((song, created) => {
         models.Candidate.findOrCreate( {
             where: { roomId: candidate.room, songId: song.get({ plain: true}).id }, defaults: { vote_count: candidate.votes }
-        }).spread((candidate, created) => {
+        }).spread((newCandidate, created) => {
             // if candidate exists
             if (created === false) {
                 // update total vote_count
-                // From Adam: Don't use eval here, not sure what you're trying to do but eval is bad
-                new_vote_count  = eval(candidate.vote_count) + eval(candidate.body.vote_count);
-                candidate.update({
+                new_vote_count  = newCandidate.vote_count + candidate.votes;
+                newCandidate.update({
                     vote_count: new_vote_count
                 });
             }
